133ff60246b16dc4cd8d44a74b69b2f6220c1d9f
mariakancheva <lactacaramel@gmail.com>
2017-10-24 18:47:24 +0300

    Stage 6
    Task 6-2 Detailed View

--- scripts/prodavachnik.js
+++ scripts/prodavachnik.js
@@ -197,7 +197,7 @@ function startApp() {
             method: "GET",
             url: kinveyAdvertsUrl,
             headers: kinveyAuthHeaders,
-            success: loadAdvertsSuccess.
+            success: loadAdvertsSuccess,
             error:handleAjaxError
         });
 
@@ -209,6 +209,7 @@ function startApp() {
                 let advertsTable = $('<table>')
                     .append($('<tr>').append(
                         '<th>Title</th>',
+                        '<th>Description</th>',
                         '<th>Publisher</th>',
                         '<th>Date Published</th>',
                         '<th>Price</th>',
@@ -216,6 +217,9 @@ function startApp() {
                     );
 
                 for (let advert of adverts) {
+                  let readMoreLink = $(`<a data-id="${advert._id}" href="#">[Read More]</a>`)
+                        .click(function() { displayAdvert($(this).attr("data-id")) });
+
                   let links = [];
 
                     if (advert._acl.creator == sessionStorage['userId']) {
@@ -228,10 +232,11 @@ function startApp() {
 
                     advertsTable.append($('<tr>').append(
                         $('<td>').text(advert.title),
+                        $('<td>').text(advert.description),
                         $('<td>').text(advert.publisher),
                         $('<td>').text(advert.datePublished),
                         $('<td>').text(advert.price),
-                        $('<td').append(links)
+                        $('<td>').append(readMoreLink))
                     ));
                 }
 
@@ -239,6 +244,41 @@ function startApp() {
             }
         }
     }
+    
+    function displayAdvert(advertId){
+            const kinveyAdvertUrl = kinveyBaseUrl + "appdata/" +
+                kinveyAppKey + "/adverts/" + advertId;
+            const kinveyAuthHeaders = {
+                'Authorization': "Kinvey " + sessionStorage.getItem('authToken'),
+            };
+
+            $.ajax({
+                method: "GET",
+                url: kinveyAdvertUrl,
+                headers: kinveyAuthHeaders,
+                success: displayAdvertSuccess
+            });
+
+            $('#viewDetailsAd').empty();
+
+            function displayAdvertSuccess(advert) {
+                let advertInfo = $('<div>').append(
+                    $('<img>').attr("src", advert.image),
+                    $('<br>'),
+                    $('<label>').text('Title:'),
+                    $('<h1>').text(advert.title),
+                    $('<label>').text('Description:'),
+                    $('<p>').text(advert.description),
+                    $('<label>').text('Publisher:'),
+                    $('<div>').text(advert.publisher),
+                    $('<label>').text('Date:'),
+                    $('<div>').text(advert.datePublished));
+
+                $('#viewDetailsAd').append(advertInfo);
+
+                showView('viewDetailsAd');
+            }
+        }
 
     // advertisement/create
     function createAdvert() {
